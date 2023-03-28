8b1b16ea9201eaf32e74390b69b19fc83cbab460
Ondřej Veselý <ondrej.vesely@sequenex.com>
2023-03-28 04:12:27 +0200

    Fix whitespaces

--- CleanArchTemplateTests/CleanArchTemplateTests.swift
+++ CleanArchTemplateTests/CleanArchTemplateTests.swift
@@ -13,19 +13,20 @@ final class CleanArchTemplateTests: XCTestCase {
     @MainActor
     func testDownloadDetailViewFeature() async {
         let store = TestStore(initialState: DownloadDetailViewFeature.State(), reducer: DownloadDetailViewFeature(downloadImage: DownloadImageInteractor()))
-        
+
         await store.send(.increaseSomething) {
             $0.something = 1
         }
-        
+
         await store.send(.downloadTapped) {
             $0.loadingsCounter = 1
         }
-        
+
         await store.receive(.downloadResult(.failure(DownloadError.invalidIdentifier)), timeout: 1) {
             $0.shouldDisplayError = true
             $0.loadingsCounter = 0
         }
+
         await store.send(.binding(.set(\.$shouldDisplayError, false))) {
             $0.shouldDisplayError = false
         }
